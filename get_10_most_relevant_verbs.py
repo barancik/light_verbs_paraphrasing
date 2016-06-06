68a4f651eb7cd83cd5ad431ce6f686b786ef6e5b
Petra Barancikova <barancikova@amalka.ms.mff.cuni.cz>
2016-06-06 18:32:45 +0200

    small move forward to the annotation

--- get_10_most_relevant_verbs.py
+++ get_10_most_relevant_verbs.py
@@ -17,27 +17,29 @@ import sys
 import gensim
 
 FILE="new_corpora"
+TAGGER = Tagger.load("/net/projects/morphodita/models/czech-morfflex-pdt-131112/czech-morfflex-pdt-131112.tagger-best_accuracy")
 
 def load_model(name,path=FILE):
     return gensim.models.Word2Vec.load(name)
 
 def return_most_similar(model,phrase):
-    similar = model.most_similar(positive=[phrase])
-    print "%s %s" % (phrase," ".join([g[0] for g in similar]))
+    similar = model.most_similar(positive=[phrase],topn=30)
+    lvcs = [x[0] for x in similar if "_" in x[0]][:10]
+    verbs = [x[0] for x in similar if "_" not in x[0] \
+            if tag(tagger,x[0])[0].tag.encode("utf-8").startswith("V")][:10]
+    return lvcs,verbs
 
 def get_paraphrases(name,lvc_list="found_LVC_over_100"):
     model = load_model(name)
+    i=1
     for line in open(lvc_list,"r"):
-        line = line.strip()
-        return_most_similar(model,line)
+        phrase = line.strip()
+        lvcs,verbs = return_most_similar(model,phrase)
+        print i,phrase
+        print ", ".join(verbs)
+        print ", ".join(lvcs)
 
-def short_lemma(lemma):
-    predek = re.match("([^\-_]+)[\-_\'']",lemma)
-    if predek:
-	return predek.group(1)
-    return lemma
-
-def tag(tagger,text):
+def tag(text,tagger=TAGGER):
     forms = Forms()
     lemmas = TaggedLemmas()
     tokens = TokenRanges()
@@ -49,7 +51,6 @@ def tag(tagger,text):
 
 
 sys.stderr.write('Loading dictionary: ')
-tagger = Tagger.load("/net/projects/morphodita/models/czech-morfflex-pdt-131112/czech-morfflex-pdt-131112.tagger-best_accuracy")
 name = "w2vmodel_vector_500"
 
 if not tagger:
@@ -58,6 +59,7 @@ if not tagger:
 
 if __name__ == '__main__':
     model = load_model(name)
+    get_paraphrases(model)
     
     
     
