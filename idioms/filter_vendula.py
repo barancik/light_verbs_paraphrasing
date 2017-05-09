7ceacf14d92d359cc105c8923ee76d88b563913b
Petra Barancikova <barp@centrum.cz>
2017-05-09 08:36:43 +0200

    fixing holes

--- idioms/filter_vendula.py
+++ idioms/filter_vendula.py
@@ -10,20 +10,11 @@ import pickle
 
 VALLEX_FILE = "/home/pida/vallex/trunk/aktualni_data/data-txt/vallex.txt"
 
-class MWE(object):
-    def __init__(self,verbs):
-        self.verbs = verbs
-        self.joint_verbs = "_".join(verbs)
-        self.dphrs = set([])
-
-    def add_dphrs(self,dphrs):
-        self.dphrs.update(dphrs)
-
 class new_MWE(object):
     def __init__(self,verbs):
         self.dphrs = set([])
         self.reflexive = defaultdict(set)
-        self.joint_verbs = "_".join(verbs)
+        self.joint_verbs = "_".join("_".join(verbs).split())
 
     def add_dphrs(self,dphrs,reflexive=False):
         if reflexive:
@@ -67,8 +58,8 @@ def extract(vallex_file=VALLEX_FILE):
     return found
 
 
-def from_vendula():
-    found = pickle.load(open("new_mwe.pickle", "rb"))
+def from_vendula(found):
+#    found = pickle.load(open("new_mwe.pickle", "rb"))
     FILE="vendula_remove.txt"
     for line in open(FILE,"r"):
         print(line.strip())
@@ -76,22 +67,31 @@ def from_vendula():
         verbs = [Verb(x.strip()) for x in tabs[0].split(",")]
         phrase = tabs[1]
         for verb in verbs:
+            to_remove = []
             if verb.reflexive:
-                found[verb.lemma].reflexive.discard(phrase)
-                if not found[verb.lemma].reflexive:
-                    found[verb.lemma].pop(verb.reflexive)
+                for i in found[verb.lemma].reflexive.keys():
+                     found[verb.lemma].reflexive[i].discard(phrase)
+                     if not found[verb.lemma].reflexive[i]:
+                         to_remove.append(i)
+                for i in to_remove:
+                    found[verb.lemma].reflexive.pop(i)
             else:
                 found[verb.lemma].dphrs.discard(phrase)
-        import pdb; pdb.set_trace()
-        if not found[verb.lemma].reflexive and not found[verb.lemma].dphrs:
-            found.pop(verb.lemma)
-            print("Removing %s" % verb.lemma)
+        #import pdb; pdb.set_trace() 
+        for verb in verbs:
+            if not found[verb.lemma].reflexive and not found[verb.lemma].dphrs:
+                found.pop(verb.lemma)
+                print("Removing %s" % verb.lemma)
+    found.pop("být")
     return found
 
-
-
-
+def main():
+    x = extract("/home/pida/vallex/trunk/aktualni_data/data-txt/v-vallex.txt")
+    y = extract("/home/pida/vallex/trunk/aktualni_data/data-txt/v-vallex-lvc.txt")
+    x.update(y)
+    filtered = from_vendula(x)
+    pickle.dump(x,open("new_mwe.pickle","wb"))
+    return x
 
 if __name__ == "__main__":
-    x=from_vendula()
-    import pdb; pdb.set_trace()
+    x=main()
